8c4c9d766ab406890492c18d026000f06c2c7aff
damstaylor <damiensabo@live.fr>
2023-11-15 16:08:27 +0100

    Create Grid.vue & GridItem.vue components

---
+++ components/Grid.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "Grid",
+  props : {
+    pictures: { type: Array, default: () => [] },
+    limit: { type: Number, default: () => 9 },
+  },
+})
+</script>
+
+<template>
+  <div class="pictures" :class="'grid-template-col-' + limit">
+    <GridItem v-for="(pic, idx) in pictures" :catData="pic" :key=idx />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/GridItem.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "GridItem",
+  props: {
+    catData: { type: Object, default: () => ({}) },
+  },
+  computed: {
+    getBreedInfo(): object {
+      return this.catData?.breeds?.[0] ?? null;
+    },
+    getBreedName(): string {
+      return this.getBreedInfo?.name ?? "Unknown breed";
+    },
+    getOrigin(): string {
+      return this.getBreedInfo?.origin ?? "Unknown origin";
+    },
+    getWikiUrl(): string {
+      return this.getBreedInfo?.wikipedia_url ?? "";
+    },
+  },
+})
+</script>
+
+<template>
+  <div class="image-container">
+    <img :src="catData.url || ''" :alt="getBreedName">
+    <div class="picture-info">
+      <a v-if="getWikiUrl" :href="getWikiUrl" target="_blank">{{ getBreedName }}</a>
+      <span v-else>{{ getBreedName }}</span>
+      <span> ({{ getOrigin }})</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
